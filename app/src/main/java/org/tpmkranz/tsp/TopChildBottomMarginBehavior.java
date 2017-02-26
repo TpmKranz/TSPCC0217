a272213b442440d496e7ccae3bbc6368810ad41d
Tom Kranz <tom@tpmkranz.org>
2017-02-26 15:12:04 +0100

    Fix second FAB's layout

--- app/src/main/java/org/tpmkranz/tsp/TopChildBottomMarginBehavior.java
+++ app/src/main/java/org/tpmkranz/tsp/TopChildBottomMarginBehavior.java
@@ -2,9 +2,11 @@ package org.tpmkranz.tsp;
 
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.CoordinatorLayout.LayoutParams;
 import android.support.design.widget.FloatingActionButton;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.View;
 
 public class TopChildBottomMarginBehavior extends FloatingActionButton.Behavior {
 
@@ -15,9 +17,22 @@ public class TopChildBottomMarginBehavior extends FloatingActionButton.Behavior 
   @Override
   public boolean onLayoutChild(CoordinatorLayout parent, FloatingActionButton child,
       int layoutDirection) {
-    child.setTranslationY(
-        -parent.getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin)
-    );
+    LayoutParams p = (LayoutParams) child.getLayoutParams();
+    try {
+      View anchor = parent.findViewById(p.getAnchorId());
+      if (anchor.getVisibility() == View.VISIBLE) {
+        child.setTranslationY(
+            -parent.getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin)
+        );
+      } else {
+        Log.d("ANCHOR VISIBILITY", String.valueOf(anchor.getVisibility()));
+        child.setTranslationY(
+            anchor.getHeight() / 2.f
+        );
+      }
+    } catch (NullPointerException e) {
+
+    }
     return super.onLayoutChild(parent, child, layoutDirection);
   }
 }
